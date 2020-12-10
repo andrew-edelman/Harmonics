e2cb078cd6f7826734c9281a2234992ba16274a7
andrew-edelman <59546220+andrew-edelman@users.noreply.github.com>
2020-12-10 15:13:04 -0600

    lots of changes, working user authentication, dynamic navbar, create gigs only for authenticated users venues

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const _ = require('lodash');
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const flash = require('connect-flash');
 
 const app = express();
 
@@ -24,6 +26,7 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 //Mongoose DB Setup
 
@@ -33,28 +36,28 @@ mongoose.connect("mongodb://localhost:27017/harmonicsDB", {useNewUrlParser: true
 mongoose.set("useCreateIndex", true);
 
 //Create collection schema
+
 const gigSchema = new mongoose.Schema ({
   date: {
     type: Date,
     required: [true]
   },
   genre: String,
-  onBill: String
 });
 
 const venueSchema = new mongoose.Schema ({
   venueName: {
-	type: String,
-	required: [true]
+	   type: String,
+	   required: [true]
   },
-  address: String,
-  bookingEmail: {
-    type: String,
-    required: [true]
+  venueBookingEmail: {
+	   type: String,
+	   required: [true]
   },
+  venueAddress: String,
   venueWebsite: String,
   gigs: [gigSchema],
-  aboutVenue: String
+  venueAbout: String
 });
 
 const bandSchema = new mongoose.Schema ({
@@ -62,22 +65,41 @@ const bandSchema = new mongoose.Schema ({
     type: String,
     required: [true]
   },
-  genre: String,
-  bandWebsite: String,
   bandEmail: String,
-  linkToMusic: String,
-  aboutBand: String
+  bandGenre: String,
+  bandWebsite: String,
+  bandMusic: String,
+  gigs: [gigSchema],
+  bandAbout: String
 });
 
-const userSchema = new mongoose.Schema ({
-  email: String,
-  password: String
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: [true]
+  },
+  password: {
+    type: String,
+    require: [true]
+  },
+  firstName: String,
+  lastName: String,
+  bands: [bandSchema],
+  venues: [venueSchema]
 });
 
 userSchema.plugin(passportLocalMongoose);
 
+
 //Create model and collection using a schema
 //The collection name should be singular and capitalized, mongoose will make it plural and uncapitalize the name
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy({passReqToCallback: true}));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const Venue = mongoose.model("Venue", venueSchema);
 
@@ -85,75 +107,32 @@ const Gig = mongoose.model("Gig", gigSchema);
 
 const Band = mongoose.model("Band", bandSchema);
 
-const User = mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-//Create root route
+//Create root Route
 
 app.get("/", function(req, res){
+  res.render("landing", {currentUser: req.user});
+});
+
+//Create route for gigs list
+
+app.get("/gigslist", function(req, res){
+
   Venue.find({}, function(err, foundVenues){
-    res.render("gigslist", {venueList: foundVenues});
+
+    res.render("gigslist", {venueList: foundVenues, currentUser: req.user});
   });
 
 });
 
-//New Venue Creation Route
-
-app.route("/newvenue")
-.get(function(req, res){
-
-  res.render("newvenue");
-
-})
-.post(function(req, res){
-
-  const newVenue = new Venue ({
-    venueName: req.body.newVenueName,
-    address: req.body.newVenueAddress,
-    bookingEmail: req.body.newVenueEmail,
-    venueWebsite: req.body.newVenueWebsite,
-    aboutVenue: req.body.newVenueAbout
-  });
-
-  newVenue.save();
-
-  res.redirect("/");
-});
-
-//New Band Creation Route
-
-app.route("/newband")
-.get(function(req, res){
-
-  res.render("newband");
-
-})
-.post(function(req, res){
-
-  const newBand = new Band ({
-    bandName: req.body.newBandName,
-    genre: req.body.newBandGenre,
-    bandWebsite: req.body.newBandWebsite,
-    bandEmail: req.body.newBandEmail,
-    linkToMusic: req.body.newBandMusic,
-    aboutBand: req.body.newBandAbout
-  });
-
-  newBand.save();
-
-  res.redirect("/");
-});
 
 //route for accessing the list of all venues
 
 app.route("/venues")
 .get(function(req, res){
+
   Venue.find({}, function(err, foundVenues){
-    res.render("venues", {venueList: foundVenues});
+    res.render("venues", {venueList: foundVenues, currentUser: req.user});
   });
 });
 
@@ -161,10 +140,23 @@ app.route("/venues")
 
 app.route("/venues/:venueName")
 .get(function(req, res){
-  const foundVenueName = req.params.venueName;
-  console.log(foundVenueName);
+  var foundVenueName = req.params.venueName;
+
   Venue.findOne({venueName: foundVenueName}, function(err, foundVenue){
-    res.render("venuepage", {venue: foundVenue});
+    console.log(foundVenue);
+    User.findOne({venues: {$elemMatch: {venueName: foundVenueName}}}, function(err, venueUser){
+      console.log(venueUser);
+      var myVenue = _.isEqual(venueUser._id, req.user._id);
+      if (myVenue == true){
+        console.log("my venue");
+        res.render("venuepage", {venue: foundVenue, currentUser: req.user, myVenue: myVenue});
+
+      } else {
+        console.log("not my venue");
+        res.render("venuepage", {venue: foundVenue, currentUser: req.user, myVenue: myVenue});
+
+      }
+    });
   });
 
 });
@@ -173,18 +165,17 @@ app.route("/venues/:venueName")
 
 app.route("/venues/:venueName/creategig")
 .get(function(req, res){
-  res.render("creategig", {venueName: req.params.venueName});
+  res.render("creategig", {venueName: req.params.venueName, currentUser: req.user});
 })
 .post(function(req, res){
   const newGig = new Gig ({
     date: req.body.newGigDate,
     genre: req.body.newGigGenre,
-    onBill: req.body.newGigOnBill
   });
 
   newGig.save();
 
-  var pushGig = {date: req.body.newGigDate, genre: req.body.newGigGenre, onBill: req.body.newGigOnBill};
+  var pushGig = {date: req.body.newGigDate, genre: req.body.newGigGenre};
 
   Venue.updateOne({venueName: req.params.venueName}, {$push: {gigs: pushGig}}, function(err){
     if (!err) {
@@ -197,12 +188,98 @@ app.route("/venues/:venueName/creategig")
 
 });
 
+//Create a new band route
+
+app.route("/createband")
+.get(function(req, res){
+  res.render("createband", {currentUser: req.user});
+})
+.post(function(req, res){
+
+  const newBand = new Band ({
+    bandName: req.body.bandName,
+    bandEmail: req.body.bandEmail,
+    bandGenre: req.body.bandGenre,
+    bandWebsite: req.body.bandWebsite,
+    bandMusic: req.body.bandMusic,
+    bandAbout: req.body.bandAbout
+  });
+
+  newBand.save();
+
+  var pushBand = {bandName: req.body.bandName,
+  bandEmail: req.body.bandEmail,
+  bandGenre: req.body.bandGenre,
+  bandWebsite: req.body.bandWebsite,
+  bandMusic: req.body.bandMusic,
+  bandAbout: req.body.bandAbout};
+
+  User.updateOne({_id: req.user._id}, {$push: {bands: pushBand}}, function(err){
+    if (!err) {
+      console.log("band successfully created");
+    } else {
+      console.log(err);
+    }
+  });
+
+  res.redirect("/profile/" + req.user._id);
+
+});
+
+//Create a new venue route
+
+app.route("/createvenue")
+.get(function(req, res){
+  res.render("createvenue", {currentUser: req.user});
+})
+.post(function(req, res){
+
+  const newVenue = new Venue ({
+    venueName: req.body.venueName,
+    venueBookingEmail: req.body.venueBookingEmail,
+    venueAddress: req.body.venueAddress,
+    venueWebsite: req.body.venueWebsite,
+    venueAbout: req.body.venueAbout
+  });
+
+  newVenue.save();
+
+  var pushVenue = {venueName: req.body.venueName,
+  venueBookingEmail: req.body.venueBookingEmail,
+  venueAddress: req.body.venueAddress,
+  venueWebsite: req.body.venueWebsite,
+  venueAbout: req.body.venueAbout};
+
+  User.updateOne({_id: req.user._id}, {$push: {venues: pushVenue}}, function(err){
+    if (!err) {
+      console.log("venue successfully created");
+    } else {
+      console.log(err);
+    }
+  });
+
+  res.redirect("/venues/" + req.body.venueName);
+
+});
+
+//User profile  page
+app.route("/profile/:_id")
+.get(function(req, res){
+  const findUserID = req.user._id;
+  User.findOne({_id: findUserID}, function(err, foundUser){
+    res.render("profile", {currentUser: foundUser});
+  });
+})
+.post(function(req, res){
+
+});
+
 //User Authentication Routes
 app.route("/login")
 .get(function(req, res){
-  res.render("login");
+  res.render("login", {flash: req.flash('error'), currentUser: req.user});
 })
-.post(function(req, res){
+.post(function(req, res, next){
 
   const user = new User({
     username: req.body.username,
@@ -212,9 +289,10 @@ app.route("/login")
   req.login(user, function(err){
     if (err){
       console.log(err);
-    } else{
+      res.redirect("/login");
+    } else {
       passport.authenticate("local")(req, res, function(){
-        res.redirect("/");
+        res.redirect("/profile/" + req.user._id);
       });
     }
   });
@@ -223,17 +301,22 @@ app.route("/login")
 
 app.route("/register")
 .get(function(req, res){
-  res.render("register");
+  res.render("register", {currentUser: req.user});
 })
 .post(function(req, res){
 
-  User.register({username: req.body.username}, req.body.password, function(err, user){
+  User.register(
+    {username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName},
+    req.body.password,
+    function(err, user){
     if (err){
       console.log(err);
       res.redirect("/register");
     } else {
       passport.authenticate("local")(req, res, function(){
-        res.redirect("/");
+        res.redirect("/profile/" + req.user._id);
       });
     }
   });
@@ -245,62 +328,6 @@ app.route("/logout")
   req.logout();
   res.redirect("/");
 });
-
-//Create starter data
-
-// const gig1 = new Gig({
-//   date: 1/1/2021,
-//   genre: "Rock",
-//   onBill: "Sweet Chicle"
-// });
-//
-// gig1.save();
-//
-// const martyrs = new Venue({
-//   venueName: "Martyrs",
-//   address: "3855 N Lincoln Ave, Chicago, IL, 60613",
-//   bookingEmail: "brenna@martyrslive.com",
-//   venueWebsite: "martyrslive.com",
-//   gigs: gig1,
-//   aboutVenue: "great venue"
-// });
-//
-// martyrs.save();
-//
-// const sweetchicle = new Band({
-//   bandName: "Sweet Chicle",
-//   genre: "Blues, Rock, Funk",
-//   bandWebsite: "sweetchicle.com",
-//   bandEmail: "sweetchicleband@gmail.com",
-//   linkToMusic: "sweetchicle.com",
-//   aboutBand: "great band"
-// });
-//
-// sweetchicle.save();
-//
-// Gig.insertMany([gig1], function(err){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     console.log("successfully saved gig");
-//   }
-// });
-//
-// Venue.insertMany([martyrs], function(err){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     console.log("successfully saved venue");
-//   }
-// });
-//
-// Band.insertMany([sweetchicle], function(err){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     console.log("successfully saved band");
-//   }
-// });
 
 //Create server port
 
